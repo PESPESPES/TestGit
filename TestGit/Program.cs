17a4d5dc7952153706412dd9198dad2d7c340a30
Gustavo Cuadra <pezmar13@gmail.com>
2018-04-24 23:14:20 -0500

    Commit 1

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -17,6 +17,7 @@ namespace TestGit
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //Commit 1
         }
     }
 }
